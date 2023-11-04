8b76022f96f0af58c8451df00636e0662fb2546a
unknown <riasat01@outlook.com>
2023-11-04 12:32:56 +0600

    api secured with jwt

--- phone-shop-server/index.js
+++ phone-shop-server/index.js
@@ -4,13 +4,41 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require("jsonwebtoken");
+const CookieParser = require('cookie-parser');
 
-// middleware
+// built in middleware
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
+app.use(CookieParser());
 
+// custom middleware
 
+const logger = async (req, res, next ) => {
+  console.log('CAlled ', req.hostname, req.originalUrl);
+  next();
+}
+
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log(`Value of token in middleware ${token}`);
+  if(!token){
+    return res.status(401).send({ message: 'Not Authorized' });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err){
+      console.log(err);
+      return res.status(401).send({message: 'unauthorized'})
+    }
+    console.log('Value in the token', decoded);
+    req.user = decoded;
+    next();
+  })
+}
 
 // mongo db
 
@@ -30,15 +58,40 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    // auth related api 
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      // console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+      res
+      .cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+      })
+      .send({success: true});
+    })
+
+    // services realated api
     const phoneCollection = client.db("phoneDB").collection("phones");
     const favouritesCollection = client.db("phoneDB").collection("favourites");
+
+
+
+    // Phones related api end points
 
     app.get('/phones', async(req, res) => {
         const result = await phoneCollection.find().toArray();
         res.send(result);
     })
 
-    app.get('/phones/:id', async(req, res) => {
+    app.get('/phones/:id', logger, verifyToken, async(req, res) => {
+        // console.log('cookies test', req.cookies)
+        const email = req.query.email;
+        // console.log(email);
+        if(email !== req.user.email){
+          return res.status(403).send({message: 'forbidden access'});
+        }
         const id = req.params.id;
         const query = {id: id};
         const options ={
@@ -47,7 +100,12 @@ async function run() {
         const result = await phoneCollection.findOne(query, options);
         res.send(result);
     })
-    app.get('/favourites', async(req, res) => {
+
+
+
+    // favourites related api end points
+
+    app.get('/favourites',  async(req, res) => {
       const result = await favouritesCollection.find().toArray();
       res.send(result);
     })
@@ -56,9 +114,13 @@ async function run() {
       const result = await favouritesCollection.insertOne(data);
       res.send(result);
     })
-    app.get('/favourites/:email', async(req, res) => {
-      const email = req.params.email;
+    app.get('/favourites/:email', logger, verifyToken, async(req, res) => {
+      const email = req.params?.email;
+      if(email !== req.user?.email){
+        return res.status(403).send({message: 'forbidden access'})
+      }
       const result = await favouritesCollection.find({email: email}).toArray();
+      console.log('User in the verified token', req.user.email);
       res.send(result);
     })
     app.delete('/favourites/:email', async(req, res) => {
@@ -67,6 +129,8 @@ async function run() {
       const result = await favouritesCollection.deleteMany(query);
       res.send(result);
     })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
